eb824a8f6bbd8cf61e3e35bce2144569af33654a
Ammar <amm4r@live.com>
2021-03-01 23:55:06 +0000

    initial Rocket left right movement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,19 +21,19 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
-            print("Thrusting"); //can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up); //can thrust while rotating
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
