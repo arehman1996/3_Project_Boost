2bab979e93f329aa4e1d315a6066475d232913e3
Ammar <amm4r@live.com>
2021-03-03 01:09:51 +0000

    Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
+  
     // Update is called once per frame
     void Update()
     {
@@ -24,16 +27,26 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             rigidBody.AddRelativeForce(Vector3.up); //can thrust while rotating
+            if (!audioSource.isPlaying) //so it doesnt layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
 
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward);
+            }
+
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward);
+            }
     }
 }
